2dcc57c4fa14d2ed23c1b1f56ccf1e4e071c493f
omkar chavan <chavanomkar804@gmail.com>
2026-03-29 12:32:52 +0530

    connected frontend to deployed backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,7 +5,7 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_API_URL || "https://cinemaiq.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
